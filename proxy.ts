edfd60150812b23fd64e6aab50d385acef6521bd
Omkar Deshmukh <omkarpowersolutions16@gmail.com>
2026-05-30 15:20:17 +0530

    fix proxy for vercel

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,4 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
@@ -7,7 +6,6 @@ export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
     await auth.protect();
   }
-  return NextResponse.next();
 });
 
 export const config = {
